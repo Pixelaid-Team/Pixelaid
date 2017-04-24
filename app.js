4d7bb1ab2672eecd87ea5549df00b9c1689ef0dd
devin hanaway <devinh11@gmail.com>
2017-04-24 15:32:53 -0600

    added questions

--- app.js
+++ app.js
@@ -26,6 +26,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+app.get('/canvas', (req, res) => {
+  res.render('canvas')
+})
+app.get('/questions', (req, res) => {
+  res.render('questions')
+})
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
