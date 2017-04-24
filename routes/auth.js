2fe9db840b3fa179c537c6e88f8921ad8d0d3092
Taylor Roberts <taylorlroberts7@gmail.com>
2017-04-24 15:32:00 -0600

    Early authorization work

--- routes/auth.js
+++ routes/auth.js
@@ -1,1 +1,30 @@
 const express = require('express')
+const router = express.Router()
+const knex = require('../db/knex')
+const db = require('../db/queries')
+const app = express()
+
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+app.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true })
+);
