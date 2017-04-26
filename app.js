a9c39a813b0f68718a5ef416b637286ef7361473
Taylor Roberts <taylorlroberts7@gmail.com>
2017-04-26 09:29:04 -0600

    Changed secret

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-  secret: process.env.SECRET,
+  secret: process.env.S3_SECRET,
   resave: false,
   saveUninitialized: false
   })
