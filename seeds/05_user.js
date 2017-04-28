b2bfe6f9fcf6758b799953d7cc8661e0d6910765
devin hanaway <devinh11@gmail.com>
2017-04-28 10:28:46 -0600

    adding db

--- seeds/05_user.js
+++ seeds/05_user.js
@@ -1,38 +1,38 @@
-//
-// exports.seed = function(knex, Promise) {
-//   // Deletes ALL existing entries
-//   return knex('users').del()
-//     .then(function () {
-//       // Inserts seed entries
-//       return knex('users').insert([
-//         {
-//         name: "admin",
-//         username: "admin",
-//         email: "admin@gmail.com",
-//         password: "keyboardcat",
-//         admin: true,
-//         pixel_count: 100000,
-//         picture_id: 1
-//         },
-//         {
-//           name: "devin",
-//           username: "devin-h",
-//           email: "devin@gmail.com",
-//           password: "keyboardcat",
-//           admin: false,
-//           pixel_count: 50,
-//           picture_id: 1
-//
-//         },
-//         {
-//           name: "Jordan",
-//           username: "jordan-f",
-//           email: "jordan@gmail.com",
-//           password: "keyboardcat",
-//           admin: false,
-//           pixel_count: 50,
-//           picture_id: 1
-//         }
-//       ]);
-//     });
-// };
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      // Inserts seed entries
+      // return knex('users').insert([
+      //   {
+      //   name: "admin",
+      //   username: "admin",
+      //   email: "admin@gmail.com",
+      //   password: "keyboardcat",
+      //   admin: true,
+      //   pixel_count: 100000,
+      //   picture_id: 1
+      //   },
+      //   {
+      //     name: "devin",
+      //     username: "devin-h",
+      //     email: "devin@gmail.com",
+      //     password: "keyboardcat",
+      //     admin: false,
+      //     pixel_count: 50,
+      //     picture_id: 1
+      //
+      //   },
+      //   {
+      //     name: "Jordan",
+      //     username: "jordan-f",
+      //     email: "jordan@gmail.com",
+      //     password: "keyboardcat",
+      //     admin: false,
+      //     pixel_count: 50,
+      //     picture_id: 1
+      //   }
+      // ]);
+    });
+};
--- seeds/09_answer.js
+++ seeds/09_answer.js
@@ -1,35 +1,35 @@
-//
-// exports.seed = function(knex, Promise) {
-//   // Deletes ALL existing entries
-//   return knex('answer').del()
-//     .then(function () {
-//       // Inserts seed entries
-//       return knex('answer').insert([
-//         {
-//         body: "Answer number 1",
-//         question_id: 1,
-//         user_id: 1,
-//         votes: 5,
-//         created_at: "Today",
-//         answer_username: "devin-hanaway"
-//         },
-//         {
-//         body: "answer number 2",
-//         question_id: 2,
-//         user_id: 3,
-//         votes: 5,
-//         created_at: "Today",
-//         answer_username: "devinDaDude"
-//
-//         },
-//         {
-//         body: "answe number 3",
-//         question_id: 3,
-//         user_id: 2,
-//         votes: 5,
-//         created_at: "Today",
-//         answer_username: "Taylor"
-//         }
-//       ]);
-//     });
-// };
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('answer').del()
+    .then(function () {
+      // // Inserts seed entries
+      // return knex('answer').insert([
+      //   {
+      //   body: "Answer number 1",
+      //   question_id: 1,
+      //   user_id: 1,
+      //   votes: 5,
+      //   created_at: "Today",
+      //   answer_username: "devin-hanaway"
+      //   },
+      //   {
+      //   body: "answer number 2",
+      //   question_id: 2,
+      //   user_id: 3,
+      //   votes: 5,
+      //   created_at: "Today",
+      //   answer_username: "devinDaDude"
+      //
+      //   },
+      //   {
+      //   body: "answe number 3",
+      //   question_id: 3,
+      //   user_id: 2,
+      //   votes: 5,
+      //   created_at: "Today",
+      //   answer_username: "Taylor"
+      //   }
+      // ]);
+    });
+};
