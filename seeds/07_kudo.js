abc3b652486053d44657080bda7c69451dd81e84
devin hanaway <devinh11@gmail.com>
2017-04-28 11:00:01 -0600

    cleaned up merges

--- seeds/07_kudo.js
+++ seeds/07_kudo.js
@@ -3,35 +3,35 @@ exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('kudo').del()
     .then(function () {
-      // Inserts seed entries
-      // return knex('kudo').insert([
-      //   {
-      //   body: "Hey thanks for help on the ERD's",
-      //   to_user_id: 1,
-      //   from_user_id: 1,
-      //   votes: 5,
-      //   created_at: "Today",
-      //   from: "nick",
-      //   to: "derek"
-      //   },
-      //   {
-      //   body: "Hey thanks for help on the ERD's",
-      //   to_user_id: 2,
-      //   from_user_id: 3,
-      //   votes: 5,
-      //   created_at: "Today",
-      //   from: "Lee",
-      //   to: "Molly"
-      //   },
-      //   {
-      //   body: "Hey thanks for help on the ERD'sn",
-      //   to_user_id: 3,
-      //   from_user_id: 2,
-      //   votes: 5,
-      //   created_at: "Today",
-      //   from: "devin",
-      //   to: "Taylor"
-      //   }
-      // ]);
+      Inserts seed entries
+      return knex('kudo').insert([
+        {
+        body: "Hey thanks for help on the ERD's",
+        to_user_id: 1,
+        from_user_id: 1,
+        votes: 5,
+        created_at: "Today",
+        from: "nick",
+        to: "derek"
+        },
+        {
+        body: "Hey thanks for help on the ERD's",
+        to_user_id: 2,
+        from_user_id: 3,
+        votes: 5,
+        created_at: "Today",
+        from: "Lee",
+        to: "Molly"
+        },
+        {
+        body: "Hey thanks for help on the ERD'sn",
+        to_user_id: 3,
+        from_user_id: 2,
+        votes: 5,
+        created_at: "Today",
+        from: "devin",
+        to: "Taylor"
+        }
+      ]);
     });
 };
--- seeds/08_question.js
+++ seeds/08_question.js
@@ -3,27 +3,27 @@ exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('question').del()
     .then(function () {
-      // Inserts seed entries
-      // return knex('question').insert([
-      //   {
-      //   title: "Hey thanks for help on the ERD's",
-      //   body: "hey where is the data model for users",
-      //   user_id: 1,
-      //   created_at: "Today"
-      //   },
-      //   {
-      //   title: "Hey thanks for help on the ERD's",
-      //   body: "hey where is the data model for users",
-      //   user_id: 3,
-      //   created_at: "Today"
-      //
-      //   },
-      //   {
-      //   title: "Hey thanks for help on the ERD'sn",
-      //   body: "hey where is the data model for users",
-      //   user_id: 2,
-      //   created_at: "Today"
-      //   }
-      // ]);
+      Inserts seed entries
+      return knex('question').insert([
+        {
+        title: "Hey thanks for help on the ERD's",
+        body: "hey where is the data model for users",
+        user_id: 1,
+        created_at: "Today"
+        },
+        {
+        title: "Hey thanks for help on the ERD's",
+        body: "hey where is the data model for users",
+        user_id: 3,
+        created_at: "Today"
+
+        },
+        {
+        title: "Hey thanks for help on the ERD'sn",
+        body: "hey where is the data model for users",
+        user_id: 2,
+        created_at: "Today"
+        }
+      ]);
     });
 };
